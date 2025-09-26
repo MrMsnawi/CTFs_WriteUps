a7d107549d3820d080aa29b716f17ac085dc2f83
MrMsnawi <abdlazizmsnawi@gmail.com>
2025-09-26 09:10:58 -0500

    reversing

---
+++ pwn.college/intro_to_cybersecurity/reverse_engineering/22_internal_state_mini_c/img_creator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+magic = b"cIMG"
+version = b"\x02\x00"
+width = 
+w = width.to_bytes(1, byteorder='little')
+height = 
+h = height.to_bytes(1, byteorder='little')
+
+
+with open("file.cimg", "wb") as file:
+    file.write()
